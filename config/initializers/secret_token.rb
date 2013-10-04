b32d1f4b0fa3a2e4fe5ddd551c8a775856a26733
Ryan <ryan.e.levi@gmail.com>
2013-10-04 08:39:09 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Domatio::Application.config.secret_token = '125946efc07b69937822bdd5b4793297d06b4dbbd3cb996295c28a54e2e2ec268846443acb24b0b187d1ded76e9c57d4f919c6db7b1d78119b26d2c2ef10f963'

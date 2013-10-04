f22b786f421925352365b32624ea15472a7c4ff3
Ryan <ryan.e.levi@gmail.com>
2013-10-04 09:38:22 -0700

    added accent to a in domatio; created about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Domatio::Application.routes.draw do
+  get "home/about"
+  match '/about' => 'home#about'
+
   get "home/index"
 
   # The priority is based upon order of creation:
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -9,4 +9,11 @@ describe HomeController do
     end
   end
 
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
 end

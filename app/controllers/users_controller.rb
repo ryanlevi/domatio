e399f459d7c92bee37992644fa81fc7978d097ae
Marc Juchli <marc@mjnet.ch>
2013-10-06 22:16:24 -0700

    Email configuration (just used my mailserver which is the easiest way for development since smtp will work when internet is available). Added a Info-Mail after successful user registration. (Doesn't work yet?)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      ActionMailer::Base.mail(:from => "info@domatio.com", :to => User.email, :subject => "Welcome to Domatio!", :body => "Congratulations, you've successfully registered at Domatio.").deliver
       redirect_to root_url, :notice => "Signed Up!"
     else
       render "new"

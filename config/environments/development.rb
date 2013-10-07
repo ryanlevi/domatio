66e9bb06fe6165bb3ec5b258af0c1439f3ac4ce8
Marc Juchli <marc@mjnet.ch>
2013-10-06 22:27:10 -0700

    Revert "Email configuration (just used my mailserver which is the easiest way for development since smtp will work when internet is available)."
    This reverts commit e399f459d7c92bee37992644fa81fc7978d097ae.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,20 +14,7 @@ Domatio::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = true
-
-  # set delivery method to :smtp, :sendmail or :test
-  config.action_mailer.delivery_method = :smtp
-
-# these options are only needed if you choose smtp delivery
-  config.action_mailer.smtp_settings = {
-      :address        => 'mail.hostag.ch',
-      :port           => 25,
-      :domain         => 'domatio.com',
-      :authentication => :login,
-      :user_name      => 'cs48@marcjuch.li',
-      :password       => 'domatio'
-  }
+  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log

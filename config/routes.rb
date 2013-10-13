d600642e01c47b951b25493e0501e02aa10f9ca0
Shayon <shayon@ubuntu.ubuntu-domain>
2013-10-12 18:35:52 -0700

    Added Groups Model, Controller, and View
    Added the Model for Groups and included a function to generate random and unique tokens.  Added the Controller for Groups with functions new and create.  Added a View for Groups called new.html.erb where a logged in user is able to create a group and is then added to the group created.  Also, added current_group to application controller.  Added some comments to the code I created.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Domatio::Application.routes.draw do
 
+  get "groups/new"
+
   # The Home Page
   get "home/index"
   root :to => 'home#index'
@@ -18,5 +20,8 @@ Domatio::Application.routes.draw do
   # The Reset passwords page
   resources :password_resets
   get "password_resets/new"
+
+  # The Group Page
+  resources :groups
 
 end

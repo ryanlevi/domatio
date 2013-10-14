5b77cd5dea54d477ffa9460b74c8f0fc23a78731
Ryan <ryan.e.levi@gmail.com>
2013-10-13 21:55:00 -0700

    Some form validation and indentation cleanup

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,41 +1,43 @@
 class GroupsController < ApplicationController
 
   def new
-  @group = Groups.new
+    @group = Groups.new
   end
 
   def create
-  @group = Groups.new(params[:groups])  #creates a group object with the values in params derived from the form located in new.html.erb for groups
-  if @group.save  #checks if the group object is save-able, if it is, enter the if-statement.
-
-    current_user.groupid=@group.groupid  #essentially puts the user who created the group, into the group.
-    current_user.save!
-    redirect_to root_url, :notice => "You've created the group: #{@group.groupname}! Try inviting some friends!"
-  else
-    render "new"  #if the group was not save-able, we re-load groups/new
-  end
+    @group = Groups.new(params[:groups])  #creates a group object with the values in params derived from the form located in new.html.erb for groups
+    if @group.save  #checks if the group object is save-able, if it is, enter the if-statement.
+      current_user.groupid=@group.groupid  #essentially puts the user who created the group, into the group.
+      current_user.save!
+      redirect_to root_url, :notice => "You've created the group: #{@group.groupname}! Try inviting some friends!"
+    else
+      render "new"  #if the group was not save-able, we re-load groups/new
+    end
   end
 
   def add_user
-  @friend=User.new
+    @friend = User.new
   end
 
   def add_user_create
-  email=params[:user][:email]
-  roommate=User.find_by_email(email)
-  if(roommate)
-    if(roommate.groupid)
-      @friend=User.new
-      redirect_to "/groups/add_user" , :notice => "That person is already in a group!  They must leave before they can join your group."
+    email=params[:user][:email]
+    roommate=User.find_by_email(email)
+    if(roommate)
+      if(roommate.groupid)
+        redirect_to "/groups/add_user" , :notice => "That person is already in a group!  They must leave before they can join your group."
+      else
+        roommate.groupid = current_group.groupid
+        roommate.save!
+        redirect_to "/groups/add_user", :notice => "#{roommate.name} was added to your group!"
+      end
     else
-      roommate.groupid = current_group.groupid
-      roommate.save!
-      redirect_to "/groups/add_user", :notice => "#{roommate.name} was added to your group!"
+      if email =~ /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+        UserMailer.group_and_site_invite(email, current_user).deliver
+        redirect_to "/groups/add_user", :notice => "An invitation was sent to #{email}!"
+      else
+        redirect_to "/groups/add_user", :notice => "Invalid email!"
+      end
     end
-  else
-    UserMailer.group_and_site_invite(email, current_user).deliver
-    redirect_to "/groups/add_user", :notice => "An invitation was sent to #{email}!"
-  end
   end
 
 end

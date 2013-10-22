8e17a8305b6622b1d9035a842390a536e23f04cd
Ryan <ryan.e.levi@gmail.com>
2013-10-21 20:55:25 -0700

    rspec shit

--- spec/controllers/bills_controller_spec.rb
+++ spec/controllers/bills_controller_spec.rb
@@ -2,25 +2,4 @@ require 'spec_helper'
 
 describe BillsController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
-    end
-  end
-
 end

02f3ef595f2a7d5e9ddc1e0570ef6805d0fb975b
Ryan <ryan.e.levi@gmail.com>
2013-10-30 00:30:01 -0700

    Added BillsHelp deletion to the destroy method (although we may change this to an automatic thing on the model end). I also added lots of comments and 1 bug fix where upon BillsHelp creation, only the last user was being added (now all are being added)

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -8,7 +8,7 @@ class BillsController < ApplicationController
       # Calculating the total number of users in the group
       @user_no = 0
       User.all.each do |user|
-        if user.groupid == current_group.groupid
+        if user.groupid == current_group.groupid # and user.name # this will take out pending users
           @user_no += 1
         end
       end
@@ -37,7 +37,7 @@ class BillsController < ApplicationController
     @bill = Bill.new
     @users = []
     User.all.each do |user|
-      if user.groupid == current_group.groupid
+      if user.groupid == current_group.groupid # and user.name # this will take out pending users
         @users.push user
       end
     end
@@ -47,22 +47,31 @@ class BillsController < ApplicationController
     @bill = Bill.find(params[:id])
     name = @bill.name
     @bill.destroy
+    # The following line finds all the rows in BillsHelp that have the same Bill ID as the one being deleted
+    # and then destroys each of them
+    BillsHelp.where("bill_id = '#{params[:id]}'").each {|bill_in_bh_table| bill_in_bh_table.destroy}
     redirect_to '/bills', :notice => "You have deleted the bill #{name}."
   end
 
   def create
+    # add a row to the Bills table with the supplied Bill Name, Amount Due and Due Date
     @bill = Bill.new(params[:bill])
+    # if the user checked recurring, store the value of the date of the month in the db
+    # if not, leave it as nil in the db
     @bill.recurring = @bill.duedate.day if params[:recurring].to_i == 1
+    # add the owner and groupid to the row in the table
     @bill.owner = current_user.email
     @bill.groupid = current_user.groupid
-    if @bill.save
+    if @bill.save # if no validation errors
+      # the following looks at the fields in the form that pertain to individual user amounts due
+      # it takes in the values you submitted and adds them row by row to the BillsHelp table
       helper_hash = {}
       params[:bills_help].each do |user, amount|
         helper_hash[:bill_id] = @bill.id
         helper_hash[:user] = user
         helper_hash[:amount] = amount
+        @helper = BillsHelp.create(helper_hash)
       end
-      @helper = BillsHelp.create(helper_hash)
       redirect_to '/bills', :notice => "You've created the bill: #{@bill.name}!"
     else
       render "new"

5849778776603dcd5e79a34d64fa22338b494f68
Ryan <ryan.e.levi@gmail.com>
2013-11-20 15:18:33 -0800

    Fixed schema

--- db/migrate/20131112031288_rewrite_schema.rb
+++ db/migrate/20131112031288_rewrite_schema.rb
@@ -1,18 +1,4 @@
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended to check this file into your version control system.
-
-ActiveRecord::Schema.define(:version => 20131119283192) do
-
+class RewriteSchema < ActiveRecord::Migration
   create_table "bills", :force => true do |t|
     t.string   "name"
     t.integer  "owner",      :limit => 255
@@ -25,21 +11,9 @@ ActiveRecord::Schema.define(:version => 20131119283192) do
     t.integer  "pending"
   end
 
-  create_table "bills_help", :force => true do |t|
-    t.integer "bill_id"
-  end
-
-  create_table "bills_helpers", :force => true do |t|
-    t.integer  "bill_id"
-    t.string   "user"
-    t.decimal  "amount"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "bills_helps", :force => true do |t|
     t.integer  "bill_id"
-    t.integer  "user"
+    t.integer   "user"
     t.decimal  "amount"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
